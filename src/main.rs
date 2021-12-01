c6682720f65615a5240cbc99b9ec5470468dcc6c
ThatcherC <j.thatcher.c@gmail.com>
2021-12-01 05:39:01 -0800

    Added channel with itunes extensions

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,27 @@
-	use rss::ChannelBuilder;
+use rss::extension::itunes::ITunesChannelExtension;
+use rss::ChannelBuilder;
 
 fn main() {
+    let channel = ChannelBuilder::default()
+        .title("Channel Title")
+        .link("http://example.com")
+        .description("An RSS feed.")
+        .build();
+    //    .unwrap();
 
-let channel = ChannelBuilder::default()
-    .title("Channel Title")
-    .link("http://example.com")
-    .description("An RSS feed.")
-    .build();
-//    .unwrap();
-    
+    let mut channelext = ITunesChannelExtension::default();
+    channelext.set_author("John Doe".to_string());
+    channelext.set_summary("Weather pod!".to_string());
+
+    let ituneschannel = ChannelBuilder::default()
+        .itunes_ext(channelext)
+        .title("Channel Title")
+        .link("http://example.com")
+        .description("An RSS feed.")
+        .build();
 
     let writer = ::std::io::stdout();
-//    channel.write_to(writer).unwrap(); // // write to the channel to a writer
+    //    channel.write_to(writer).unwrap(); // // write to the channel to a writer
 
     // pretty writer from https://docs.rs/rss/latest/rss/struct.Channel.html#example-2
     channel.pretty_write_to(writer, b' ', 2).unwrap(); // // write to the channel to a writer
@@ -20,4 +30,11 @@ let channel = ChannelBuilder::default()
     println!("");
     println!("");
     println!("{}", string);
+    println!("");
+    println!("");
+    ituneschannel
+        .pretty_write_to(::std::io::stdout(), b' ', 2)
+        .unwrap();
+    println!("");
+    println!("");
 }
