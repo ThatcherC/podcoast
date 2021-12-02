440fcfbbdbdd9636878a2037abef94c1d3bef8b6
ThatcherC <j.thatcher.c@gmail.com>
2021-12-02 10:46:52 -0800

    Added file-to-channel function

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,50 @@
 use rss::extension::itunes::ITunesChannelExtension;
 use rss::ChannelBuilder;
 
-fn main() {
-    let channel = ChannelBuilder::default()
-        .title("Channel Title")
-        .link("http://example.com")
-        .description("An RSS feed.")
-        .build();
-    //    .unwrap();
+fn channelfromfile() -> rss::ChannelBuilder{
+    let f = std::fs::File::open("channel.yaml").expect("Unable to open file!");
+    let data: serde_yaml::Value = serde_yaml::from_reader(f).expect("Unable to deserialize!");
+
+    let linkurl = data["link"]
+        .as_str()
+        .map(|s| s.to_string())
+        .expect("Could not find key link in something.yaml");
+    println!("Link: {}", linkurl);
+
+
+    let description = data["description"]
+        .as_str()
+        .map(|s| s.to_string())
+        .expect("Could not find key description in something.yaml");
+    println!("Desc: {}", description);
+
+    let title = data["title"]
+        .as_str()
+        .map(|s| s.to_string())
+        .expect("Could not find key title in something.yaml");
+    println!("Title: {}", title);
 
     let mut channelext = ITunesChannelExtension::default();
     channelext.set_author("John Doe".to_string());
-    channelext.set_summary("Weather pod!".to_string());
+    channelext.set_summary(description);
 
     let ituneschannel = ChannelBuilder::default()
         .itunes_ext(channelext)
-        .title("Channel Title")
-        .link("http://example.com")
-        .description("An RSS feed.")
-        .build();
+        .title(title)
+        .link(linkurl)
+        .clone();
+
+    return ituneschannel;
+}
+
+
+
+fn main() {
+    let ituneschannel = channelfromfile().build();
 
     let writer = ::std::io::stdout();
     //    channel.write_to(writer).unwrap(); // // write to the channel to a writer
 
-    // pretty writer from https://docs.rs/rss/latest/rss/struct.Channel.html#example-2
-    channel.pretty_write_to(writer, b' ', 2).unwrap(); // // write to the channel to a writer
-    let string = channel.to_string(); // convert the channel to a string
-
-    println!("");
-    println!("");
-    println!("{}", string);
     println!("");
     println!("");
     ituneschannel
